bc9c54d6ab54b74b5588fe928927669789a57e87
Khurshedspi <khurshed5322@gmail.com>
2024-11-13 23:08:45 +0600

    create homeLaout

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+const HomeLayout = () => {
+    return (
+        <div>
+          <Header></Header>
+            <nav/>
+            <main/>
+        </div>
+    );
+};
+
+export default HomeLayout;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import HomeLayout from "../layouts/HomeLayout";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <HomeLayout></HomeLayout>,
     },
     {
        path: "/news",
